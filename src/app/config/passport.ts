028ba589df3bda37503806e174f03c638408f394
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-07-24 11:20:59 +0600

    setup password login feature with passport

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -7,6 +7,48 @@ import {
 import envVars from "./envvars.config";
 import { User } from "../modules/user/user.model";
 import { Role } from "../modules/user/user.interfaces";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcryptjs from "bcryptjs";
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email: string, password: string, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: "User not found" });
+        }
+
+        const googleAuthenticated = user.auths.some(
+          (auth) => auth.provider === "google"
+        );
+
+        if (googleAuthenticated) {
+          return done(null, false, {
+            message: "User already authenticated with Google",
+          });
+        }
+
+        const isValidPassword = await bcryptjs.compare(
+          password,
+          user.password as string
+        );
+        if (!isValidPassword) {
+          return done(null, false, { message: "Invalid password" });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        console.log(error);
+        done(error);
+      }
+    }
+  )
+);
 
 passport.use(
   new GoogleAuthStrategy(
@@ -47,17 +89,15 @@ passport.use(
   )
 );
 
-passport.serializeUser((user: any, done: (err: any, id?: unknown) => void) => {
+passport.serializeUser((user: any, done: VerifyCallback) => {
   done(null, user._id);
 });
 
-passport.deserializeUser(
-  async (id: string, done: (err: any, user?: any) => void) => {
-    try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch (error) {
-      done(error);
-    }
+passport.deserializeUser(async (id: string, done: VerifyCallback) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user || false);
+  } catch (error) {
+    done(error);
   }
-);
+});
